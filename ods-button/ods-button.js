769cd7a39ec7abe16e08aa03e4b2f783d48fbee8
Xiaoyin Lin <Xiaoyin.Lin@alaskaair.com>
2019-01-24 13:51:05 -0800

    added different button types/states with sass extracted

--- ods-button/ods-button.js
+++ ods-button/ods-button.js
@@ -1,20 +1,49 @@
-import { LitElement, html } from 'lit-element';
-import css from './ods-button-css.js';
+import { LitElement, html } from "lit-element";
+import css from "./ods-button-css.js";
 
 class OdsButton extends LitElement {
+  constructor() {
+    super();
+    this.string = "";
+    this.type = "primary";
+    this.disabled = false;
+    this.active = false;
+    this.getButtonStyle = this.getButtonStyle.bind(this);
+  }
+
   static get properties() {
     return {
-      style: String,
-    }
+      string: {
+        type: String
+      },
+      type: {
+        type: String
+      },
+      disabled: {
+        type: Boolean
+      },
+      active: {
+        type: Boolean
+      }
+    };
+  }
+
+  getButtonStyle(active, type) {
+    if (type === "secondary") return "button--secondary";
+    return active ? "button--active" : "";
   }
 
   render() {
     return html`
       ${css}
-
-      <button class='${this.style}'><slot></slot></button>
+      <button
+        class="button ${this.getButtonStyle(this.active, this.type)}"
+        ?disabled="${this.disabled || this.active}"
+      >
+        ${this.string}
+      </button>
     `;
   }
 }
 
-customElements.define('ods-button', OdsButton);
+customElements.define("ods-button", OdsButton);
