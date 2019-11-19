f0c0df18a50dfab0e4f1e1419c2c20e516947f4d
Dale Sande <dale.sande@alaskaair.com>
2019-11-19 12:11:57 -0800

    refactor: updates per code review

--- src/ods-button-light.js
+++ src/ods-button-light.js
@@ -1,3 +1,8 @@
+// Copyright (c) Alaska Air. All right reserved. Licensed under the Apache-2.0 license
+// See LICENSE in the project root for license information.
+
+// ---------------------------------------------------------------------
+
 import { html } from "lit-element";
 import ComponentBase from './component-base';
 import buttonProperties from './tokens/componentProperties-ld-css.js';
