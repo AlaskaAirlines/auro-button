0cc40e10a0b6c9ff851301f18c998a375512596d
Dale Sande / @anotheruiguy <dale.sande@gmail.com>
2020-02-05 10:41:01 -0800

    refactor: code review refactor (#49)
    Co-authored-by: Alaska Design and Research <48228490+ecommerceUXdesign@users.noreply.github.com>

--- src/auro-component-base.js
+++ src/auro-component-base.js
@@ -5,17 +5,12 @@
 
 import { LitElement, html } from "lit-element";
 import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { classMap } from 'lit-html/directives/class-map';
 import 'focus-visible/dist/focus-visible.min.js';
 
 export default class AuroComponentBase extends LitElement {
   constructor() {
     super();
-    this.buttontype = "primary";
-    this.responsive = false;
-    this.disabled = false;
-    this.isactive = false;
-    this.getButtonType = this.getButtonType.bind(this);
-    this.getButtonState = this.getButtonState.bind(this);
 
     // adds event based on actively being touched
     this.addEventListener('touchstart', function() {
@@ -26,16 +21,14 @@ export default class AuroComponentBase extends LitElement {
   static get properties() {
     return {
       autofocus:        { type: Boolean },
-      condensed:        { type: Boolean },
-      disabled:         { type: Boolean },
       formnovalidate:   { type: Boolean },
       isactive:         { type: Boolean },
       responsive:       { type: Boolean },
       reverse:          { type: Boolean },
+      secondary:        { type: Boolean },
+      tertiary:         { type: Boolean },
       arialabel:        { type: String },
       arialabelledby:   { type: String },
-      buttontype:       { type: String },
-      flowtype:         { type: String },
       form:             { type: String },
       formaction:       { type: String },
       formenctype:      { type: String },
@@ -43,9 +36,6 @@ export default class AuroComponentBase extends LitElement {
       formtarget:       { type: String },
       id:               { type: String },
       name:             { type: String },
-      svgIconLeft:      { type: String },
-      svgIconRight:     { type: String },
-      theme:            { type: String },
       title:            { type: String },
       type:             { type: String },
       value:            { type: String },
@@ -60,30 +50,18 @@ export default class AuroComponentBase extends LitElement {
     return this.svg;
   }
 
-  getButtonState(isactive) {
-    return isactive ? "is-active" : ""
-  }
-
-
-  isDisabled(disabled, isactive) {
-    return disabled || isactive;
-  }
-
   getButtonContent() {
     return html`<slot></slot>`;
   }
 
-  getButtonType(type) {
-    const validTypes = [
-      "primary",
-     "secondary",
-     "tertiary"
-    ];
-
-    return validTypes.indexOf(type) ? type : ""
-  }
-
   render() {
+
+    const classes = {
+      'util_insetLg--squish': true,
+      'auro-button': true,
+      'auro-button--secondary': this.secondary,
+      'auro-button--tertiary': this.tertiary
+    };
 
     return html`
       ${this.getButtonStyles()}
@@ -92,13 +70,7 @@ export default class AuroComponentBase extends LitElement {
         aria-label="${ifDefined(this.arialabel ? this.arialabel : undefined)}"
         aria-labelledby="${ifDefined(this.arialabelledby ? this.arialabelledby : undefined)}"
         ?autofocus="${this.autofocus}"
-
-        class="auro-button
-          ${this.getButtonType(this.buttontype)}
-          ${this.getButtonState(this.isactive)}
-          "
-
-        ?disabled="${this.isDisabled(this.disabled, this.isactive)}"
+        class="${classMap(classes)}"
         form="${ifDefined(this.form ? this.form : undefined)}"
         formaction="${ifDefined(this.formaction ? this.formaction : undefined)}"
         formenctype="${ifDefined(this.formenctype ? this.formenctype : undefined)}"
@@ -108,19 +80,12 @@ export default class AuroComponentBase extends LitElement {
         id="${ifDefined(this.id ? this.id : undefined)}"
         title="${ifDefined(this.title ? this.title : undefined)}"
         name="${ifDefined(this.name ? this.name : undefined)}"
-        part="button--modifier"
         type="${ifDefined(this.type ? this.type : undefined)}"
         .value="${ifDefined(this.value ? this.value : undefined)}"
 
         @click="${() => {}}"
       >
-
-        ${ifDefined(this.svgIconLeft ? this.getIcon(this.svgIconLeft) : undefined)}
-
         ${this.getButtonContent()}
-
-        ${ifDefined(this.svgIconRight ? this.getIcon(this.svgIconRight) : undefined)}
-
       </button>
     `;
   }
