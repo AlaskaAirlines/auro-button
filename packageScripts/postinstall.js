157a65081a7e9900dbac7d77152f096f13ef166f
Dale Sande <dale.sande@alaskaair.com>
2020-04-23 21:51:36 -0700

    fix: error caused by unneeded postinstall script

--- packageScripts/postinstall.js
+++ packageScripts/postinstall.js
@@ -33,17 +33,3 @@ chalk.hex('#f26135')(`
 ╰─────────────────────────────── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─╯
 `)
 );
-
-const fs = require('fs');
-
-// convince Parcel to Transpile auro dependencies
-const modules = [
-  'lit-element',
-  'lit-html',
-];
-const browserslistrc = 'node 12.15.0';
-
-modules.forEach(name => {
-  fs.writeFileSync(`node_modules/${name}/.browserslistrc`, browserslistrc);
-});
-
