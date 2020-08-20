d970b71bfb4ecfef690187042ee5f73e5a20a9b7
James Jackson <james.jackson2@alaskaair.com>
2020-08-20 12:55:36 -0700

    BREAKING CHANGE: remove ods-button and add unit tests

--- docs/BUILD_STATUS.md
+++
@@ -1,3 +0,0 @@
-[![Build Status](https://img.shields.io/travis/AlaskaAirlines/ods-button.svg?branch=master&style=for-the-badge)](https://travis-ci.org/github/AlaskaAirlines/ods-button)
-[![See it on NPM!](https://img.shields.io/npm/v/@alaskaairux/ods-button.svg?style=for-the-badge&color=orange)](https://www.npmjs.com/package/@alaskaairux/ods-button)
-[![License](https://img.shields.io/npm/l/@alaskaairux/orion-design-tokens.svg?color=blue&style=for-the-badge)](https://www.apache.org/licenses/LICENSE-2.0)
--- src/auro-button.js
+++ src/auro-button.js
@@ -3,18 +3,91 @@
 
 // ---------------------------------------------------------------------
 
-import { html } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { classMap } from 'lit-html/directives/class-map';
 import 'focus-visible/dist/focus-visible.min.js';
-import AuroComponentBase from './auro-component-base';
-import styleAuroCss from "./style-auro-css.js";
+import styleCss from "./style-css.js";
 
-class AuroButton extends AuroComponentBase {
+class AuroButton extends LitElement {
+  static get styles() {
+    return css`
+      ${styleCss}
+    `;
+  }
 
-  // Adds styles for light DOM element; styles not defined in base class
-  getButtonStyles() {
+  static get properties() {
+    return {
+      autofocus:        { type: Boolean },
+      disabled:         { type: Boolean },
+      formnovalidate:   { type: Boolean },
+      ondark:           { type: Boolean },
+      secondary:        { type: Boolean },
+      tertiary:         { type: Boolean },
+      arialabel:        { type: String },
+      arialabelledby:   { type: String },
+      form:             { type: String },
+      formaction:       { type: String },
+      formenctype:      { type: String },
+      formmethod:       { type: String },
+      formtarget:       { type: String },
+      id:               { type: String },
+      name:             { type: String },
+      title:            { type: String },
+      type:             { type: String },
+      value:            { type: String },
+      svgIconLeft:      { type: String },
+      svgIconRight:     { type: String },
+    };
+  }
+
+  focus() {
+    this.renderRoot.querySelector('button').focus();
+  }
+
+  // getIcon(svgIcon) {
+  //   this.dom = new DOMParser().parseFromString(svgIcon, 'text/html');
+  //   this.svg = this.dom.body.firstChild;
+
+  //   return this.svg;
+  // }
+
+  render() {
+
+    const classes = {
+      'util_insetLg--squish': true,
+      'auro-button': true,
+      'auro-buttonOndark': this.ondark,
+      'auro-button--secondary': this.secondary,
+      'auro-buttonOndark--secondary': this.secondary && this.ondark,
+      'auro-button--tertiary': this.tertiary,
+      'auro-buttonOndark--tertiary': this.tertiary && this.ondark
+    };
+
     return html`
-      ${styleAuroCss}
-    `
+      <button
+        aria-label="${ifDefined(this.arialabel ? this.arialabel : undefined)}"
+        aria-labelledby="${ifDefined(this.arialabelledby ? this.arialabelledby : undefined)}"
+        ?autofocus="${this.autofocus}"
+        class="${classMap(classes)}"
+        ?disabled="${this.disabled}"
+        form="${ifDefined(this.form ? this.form : undefined)}"
+        formaction="${ifDefined(this.formaction ? this.formaction : undefined)}"
+        formenctype="${ifDefined(this.formenctype ? this.formenctype : undefined)}"
+        formmethod="${ifDefined(this.formmethod ? this.formmethod : undefined)}"
+        ?formnovalidate="${this.formnovalidate}"
+        formtarget="${ifDefined(this.formtarget ? this.formtarget : undefined)}"
+        id="${ifDefined(this.id ? this.id : undefined)}"
+        title="${ifDefined(this.title ? this.title : undefined)}"
+        name="${ifDefined(this.name ? this.name : undefined)}"
+        type="${ifDefined(this.type ? this.type : undefined)}"
+        .value="${ifDefined(this.value ? this.value : undefined)}"
+
+        @click="${() => {}}"
+      >
+        <slot></slot>
+      </button>
+    `;
   }
 }
 
