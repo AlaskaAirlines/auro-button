57307afa5fb77d338d5f6fb1dab3299af8250d00
Geoff Rich <geoff.rich@alaskaair.com>
2021-05-26 08:37:08 -0700

    docs: update property types

--- src/auro-button.js
+++ src/auro-button.js
@@ -21,14 +21,14 @@ import version from './version';
  * @attr {Boolean} ondark - Set value for on-dark version of auro-button
  * @attr {Boolean} secondary - Set value for secondary version of auro-button
  * @attr {Boolean} tertiary - Set value for tertiary version of auro-button
- * @attr {Boolean} arialabel - Populates the `aria-label` attribute that is used to define a string that labels the current element. Use it in cases where a text label is not visible on the screen. If there is visible text labeling the element, use `aria-labelledby` instead.
- * @attr {Boolean} arialabelledby - Populates the `aria-labelledby` attribute that establishes relationships between objects and their label(s), and its value should be one or more element IDs, which refer to elements that have the text needed for labeling. List multiple element IDs in a space delimited fashion.
- * @attr {Boolean} id - Set the unique ID of an element.
- * @attr {Boolean} title - Sets title attribute. The information is most often shown as a tooltip text when the mouse moves over the element.
- * @attr {Boolean} type - The type of the button. Possible values are: `submit`, `reset`, `button`
- * @attr {Boolean} value - Defines the value associated with the button which is submitted with the form data.
- * @attr {Boolean} svgIconLeft - **DEPRECATED** Use auro-icon
- * @attr {Boolean} svgIconRight - **DEPRECATED** Use auro-icon
+ * @attr {String} arialabel - Populates the `aria-label` attribute that is used to define a string that labels the current element. Use it in cases where a text label is not visible on the screen. If there is visible text labeling the element, use `aria-labelledby` instead.
+ * @attr {String} arialabelledby - Populates the `aria-labelledby` attribute that establishes relationships between objects and their label(s), and its value should be one or more element IDs, which refer to elements that have the text needed for labeling. List multiple element IDs in a space delimited fashion.
+ * @attr {String} id - Set the unique ID of an element.
+ * @attr {String} title - Sets title attribute. The information is most often shown as a tooltip text when the mouse moves over the element.
+ * @attr {String} type - The type of the button. Possible values are: `submit`, `reset`, `button`
+ * @attr {String} value - Defines the value associated with the button which is submitted with the form data.
+ * @attr {String} svgIconLeft - **DEPRECATED** Use auro-icon
+ * @attr {String} svgIconRight - **DEPRECATED** Use auro-icon
  *
  * @slot - Provide text for the button.
  */
