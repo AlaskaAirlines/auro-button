26a7590bdfa6af81a9c965f48e53ccb3b4324af9
Dale Sande <dale.sande@gmail.com>
2023-04-24 16:37:34 -0700

    refactor(es5): remove legacy features and build support #204
    BREAKING CHANGE: This commit removes all legacy es6 build process
    including light-dom support, fixed CSS, non CSS variable support,
    and es5 bundling.
    
    Changes to be committed:
    modified:   README.md
    deleted:    babel.config.js
    deleted:    demo/alert.js
    modified:   demo/demo.md
    modified:   demo/index.html
    deleted:    demo/sass/style.scss
    deleted:    index.html
    modified:   package-lock.json
    modified:   package.json
    modified:   packageScripts/postinstall.js
    modified:   rollup.config.js
    modified:   scripts/postCss.js
    deleted:    scripts/removeNonRemPlugin.js
    deleted:    src/auro-button-light.js
    modified:   src/auro-button.js
    deleted:    src/es5.js
    deleted:    src/style-fixed.scss
    deleted:    src/style-ld.scss

--- scripts/postCss.js
+++ scripts/postCss.js
@@ -1,59 +1,20 @@
-// Copyright (c) Alaska Air. All right reserved. Licensed under the Apache-2.0 license
-// See LICENSE in the project root for license information.
-
-// ---------------------------------------------------------------------
-
 const autoprefixer = require('autoprefixer');
 const postcss = require('postcss');
-const remToPx = require('postcss-rem-to-pixel');
-const removeNonRem = require('./removeNonRemPlugin.js');
-const postcssCustomProperties = require('postcss-custom-properties');
-const removeRules = require('postcss-remove-rules');
 const comments = require('postcss-discard-comments');
 const fs = require('fs');
 
 fs.readFile('src/style.css', (err, css) => {
-  postcss([autoprefixer, postcssCustomProperties, comments])
+  postcss([autoprefixer, comments])
     .use(comments({
-      remove: function(comment) { return comment[0] == "@"; }
-    }))
-    .use(removeRules({
-      rulesToRemove: {
-        ':root': '*'
+      remove(comment) {
+        return comment[0] == "@";
       }
     }))
     .process(css, { from: 'src/style.css', to: 'src/style.css' })
-    .then(result => {
+    .then((result) => {
       fs.writeFile('src/style.css', result.css, () => true)
-      if ( result.map ) {
+      if (result.map) {
         fs.writeFile('src/style.map', result.map, () => true)
       }
     })
-  });
-
-
-  /*
-  Output a "fixed" stylesheet that only contains declarations with rem units
-  converted to their px equivalent.
-  */
-  const fixedFile = 'src/style-fixed.css';
-  const fixedFileMap = 'src/style-fixed.map';
-  fs.readFile(fixedFile, (err, css) => {
-    postcss([
-      autoprefixer,
-      postcssCustomProperties({preserve: false}),
-      comments,
-      removeNonRem,
-      remToPx({replace: true, propList: ['*']})
-    ])
-    .use(comments({
-        remove: function(comment) { return comment[0] == "@"; }
-      }))
-      .process(css, { from: fixedFile, to: fixedFile })
-      .then(result => {
-        fs.writeFile(fixedFile, result.css, () => true)
-        if ( result.map ) {
-          fs.writeFile(fixedFileMap, result.map, () => true)
-        }
-      })
-  });
+});
