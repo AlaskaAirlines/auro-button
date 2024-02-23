0225d9bab815e4f5b2788161221480998755d37d
Jordan Jones <jmjones2463@gmail.com>
2024-02-23 13:39:39 -0800

    perf(min): update min js example system

--- demo/index.js
+++ demo/index.js
@@ -1,12 +1,8 @@
-import { toggledTextExample } from "../apiExamples/toggledText";
-import { roundedRightAlignExample } from "../apiExamples/roundedRightAlign";
 
 export function initExamples(initCount) {
   initCount = initCount || 0;
 
   try {
-    toggledTextExample();
-    roundedRightAlignExample();
   } catch {
     if (initCount <= 20) {
       // setTimeout handles issue where content is sometimes loaded after the functions get called
