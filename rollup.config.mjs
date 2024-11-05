37244c7825b45c4373972a35c50177d23227b1f8
Jordan Jones <jmjones2463@gmail.com>
2024-11-05 12:46:59 -0800

    perf: update rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -28,7 +28,8 @@ const indexExamplesConfig = {
   output: {
     format: 'esm',
     dir: 'demo/'
-  }
+  },
+  plugins: [nodeResolve()]
 };
 
 const apiExamplesConfig = {
@@ -38,7 +39,8 @@ const apiExamplesConfig = {
   output: {
     format: 'esm',
     dir: 'demo/'
-  }
+  },
+  plugins: [nodeResolve()]
 };
 
 export default [modernConfig, indexExamplesConfig, apiExamplesConfig];
