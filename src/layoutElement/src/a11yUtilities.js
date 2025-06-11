92f8d21de04fd262f544b0d02e47c0e8b149247b
Stephen Rios <Stephen.Rios@AlaskaAir.com>
2025-06-11 10:22:31 -0500

    feat: refactor a11yUtilities into reusable attribute transporter and implement in new a11yUtilities
    - Refactor a11y utilities into reusable attribute watcher/transporter utility
    - Implement new attribute transporter in new a11yUtilities
    - a11yUtilities updated to use map of attributes to match for easy configuration
    - Independent transportation utilities added
    - "All a11y attributes" utility added.

---
+++ src/layoutElement/src/a11yUtilities.js
@@ -0,0 +1,38 @@
+import { transportAttributes } from "./transportAttributes.js";
+
+const _matchers = {
+  'aria-': attr => attr.startsWith('aria-'),
+  'role': attr => attr.match(/^role$/)
+};
+
+export const transportAriaAttributes = ({ host, target, removeOriginal = true }) => {
+  return transportAttributes({
+    host,
+    target,
+    match: _matchers['aria-'],
+    removeOriginal
+  });
+};
+
+export const transportRoleAttributes = ({ host, target, removeOriginal = true }) => {
+  return transportAttributes({
+    host,
+    target,
+    match: _matchers['role'],
+    removeOriginal
+  });
+};
+
+export const transportAllA11yAttributes = ({ host, target, removeOriginal = true }) => {
+  return transportAttributes({
+    host,
+    target,
+    match: attr => {
+      for (const key in _matchers) {
+        if (_matchers[key](attr)) return true;
+      }
+      return false;
+    },
+    removeOriginal
+  });
+}
