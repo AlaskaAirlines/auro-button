2fff3f89d5c61db060882549b37fa1138437600c
Stephen Rios <Stephen.Rios@AlaskaAir.com>
2025-06-24 15:32:16 -0500

    perf: remove type attribute from wrapper property for more consistent environment support

--- src/layoutElement/src/auroElement.js
+++ src/layoutElement/src/auroElement.js
@@ -65,7 +65,6 @@ export class AuroElement extends LitElement {
        * @private
        */
       wrapper: {
-        type: HTMLElement,
         attribute: false,
         reflect: false
       }
